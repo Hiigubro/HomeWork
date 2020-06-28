d008c4451a9e3a47262643725764002edec1ee0f
Viktoryia Latushkina <latushkina.viktoryia@gmail.com>
2020-06-28 21:08:31 +0300

    hw 5 counter

---
+++ HomeWork5/CalculatorWithCounter.java
@@ -0,0 +1,58 @@
+package HomeWork5;
+
+public class CalculatorWithCounter {
+
+    private ICalculator iCalculator;
+    private int counter;
+
+    public CalculatorWithCounter (ICalculator iCalculator){
+        this.iCalculator = iCalculator;
+        counter = 0;
+    }
+
+    //сумма
+    public final double plus (double a, double b){
+        counter++;
+        return iCalculator.plus(a, b);
+    }
+
+    //разница
+    public final double minus (double a, double b){
+        counter++;
+        return iCalculator.minus(a, b);
+    }
+
+    //умножение
+    public final double mult (double a, double b){
+        counter++;
+        return iCalculator.mult(a, b);
+    }
+
+    //деление
+    public final double div (double a, double b){
+        counter++;
+        return iCalculator.div(a, b);
+    }
+
+    //возведение в степень
+    public final double power (double a, double b){
+        counter++;
+        return iCalculator.power(a, b);
+    }
+
+    //модуль числа
+    public final double module (double a){
+        counter++;
+        return iCalculator.module(a);
+    }
+
+    //извлечение квадратного корня
+    public final double squareRoot (double a){
+        counter++;
+        return iCalculator.squareRoot(a);
+    }
+
+    public final int getCountOperation(){
+        return counter;
+    }
+}
---
+++ HomeWork5/CalculatorWithCounter_Main.java
@@ -0,0 +1,35 @@
+package HomeWork5;
+
+import java.text.DecimalFormat;
+
+public class CalculatorWithCounter_Main {
+    public static void main(String[] args) {
+
+        CalculatorWithCounter calculatorWithCounter = new CalculatorWithCounter(new CalculatorWithOperator());
+        calculatorWithCounter = new CalculatorWithCounter(new CalculatorWithMathCopy());
+        calculatorWithCounter = new CalculatorWithCounter(new CalculatorWithMathExtends());
+
+        //форматирование числа
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        double a = 4.1;
+        double b = calculatorWithCounter.mult(15,7);
+        double c = calculatorWithCounter.div(28,5);
+        double d = calculatorWithCounter.power(c,2);
+        double e = calculatorWithCounter.plus(b, d);
+        double result = calculatorWithCounter.plus(a, e);
+
+        System.out.println("Число операций = " + calculatorWithCounter.getCountOperation());
+
+        System.out.println("Реализация примера с помощью CalculatorWithMathCounter: ");
+
+        // Вывод результата
+        System.out.printf("4.1 + 15 * 7 + (28 / 5) ^ 2 = " + df.format(result));
+
+        // 4. Деление сохраненного результата на 0
+        System.out.printf(" / 0 = " + df.format(result), df.format(result / 0));
+
+        // 5. Деление сохраненного new результата на 0.0d
+        System.out.printf(" / 0.0d = " + df.format(result), df.format(result / 0.0d));
+    }
+}
